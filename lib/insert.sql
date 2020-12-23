f58f826b39449de884e348a9a9841720c3fa52bd
msenior526 <msenior526@gmail.com>
2020-12-23 17:52:41 -0500

    commit tree a1f9e7e19a01791b9ff4356a0940fc0545c5a313

---
+++ lib/insert.sql
@@ -0,0 +1,53 @@
+INSERT INTO series VALUES 
+(1, "SHREK", 1, 1),
+(2, "HUNGER GAMES", 2, 2);
+
+INSERT INTO books VALUES 
+(1, "SHREK 1", 2001, 1),
+(2, "SHREK 2", 2003, 1),
+(3, "SHREK 3", 2013, 1),
+(4, "HUNGER GAMES 1", 2014, 2),
+(5, "HUNGER GAMES 2", 2015, 2),
+(6, "HUNGER GAMES 3", 2016, 2);
+
+INSERT INTO characters VALUES 
+(1, "Shrek", "Ogre", "I'm a nice ogre.", 1),
+(2, "Fiona", "Ogre", "Oh, Prince Charming", 1),
+(3, "Puss in Boots", "Cat", "Meow.", 1),
+(4, "Donkey", "Donkey", "In the morning, I'm making waffles.", 1),
+(5, "Katniss Everdeen", "Human", "I volunteer as tribute.", 2),
+(6, "Primrose Everdeen", "Human", "Please don't go.", 2),
+(7, "Rue", "Human", "I'm dead.", 2),
+(8, "Peeta", "Human", "I love you Katniss.", 2);
+
+INSERT INTO subgenres VALUES 
+(1, "Family"),
+(2, "Action & Adventure");
+
+INSERT INTO authors VALUES 
+(1, "William Steig"),
+(2, "Suzanne Collins");
+
+INSERT INTO character_books VALUES 
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 2, 1),
+(5, 2, 2),
+(6, 2, 3),
+(7, 3, 2),
+(8, 4, 3),
+(9, 5, 1),
+(10, 5, 2),
+(11, 5, 3),
+(12, 8, 1),
+(13, 8, 2),
+(14, 8, 3),
+(15, 6, 1),
+(16, 7, 2);
+
+
+
+
+
+
